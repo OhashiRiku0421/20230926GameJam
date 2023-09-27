6c99e6e90f7c78daf0f02fb12f7e3091bb3e3981
kudononoka <106291674+kudononoka@users.noreply.github.com>
2023-09-27 10:53:12 +0900

    [update]アナウンス音追加

--- Assets/Kudo/AnnouncementTextAnimationController.cs
+++ Assets/Kudo/AnnouncementTextAnimationController.cs
@@ -6,17 +6,20 @@ using UnityEngine.UI;
 public class AnnouncementTextAnimationController : MonoBehaviour
 {
     Animator _anim;
+    AudioSource _audioSource;
     Text _announcementText;
     void Start()
     {
         _anim = GetComponent<Animator>();
+        _audioSource = GetComponent<AudioSource>();
         _announcementText = GetComponent<Text>();
     }
     /// <summary>アナウンスTextのアニメーションをPlayするメソッド</summary>
     /// <param name="anaText">アナウンスのText</param>
     public void AnimationPlay(string anaText)
     {
+        _audioSource.Play();
         _announcementText.text = $"次は{anaText}";
-        _anim.SetTrigger("IsSideMove");
+        _anim.SetTrigger("SideMove");
     }
 }
